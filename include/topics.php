fabbdfe17bc6e335b1d06e63439e3ff1eae54582
tran cam le <Cuu Vui Ve@DESKTOP-NN8P9H0>
2020-09-19 22:43:30 +0700

    searchandpagingtopic

---
+++ include/topics.php
@@ -0,0 +1,119 @@
+<?php
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    include_once "database/db.inc";
+
+    $err_msg = "";
+    $conn  = getConnection();
+
+    if($conn->connect_error) {
+        die("Failed to connect to database. ".$conn->connect_error);
+    }
+
+
+    // try
+    // {   
+    //     $queryCondition = "";
+
+    //     if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    //         $title = isset($_POST["title"]) ? $_POST["title"] : '';
+    //         $status = isset($_POST["status"]) ? $_POST["status"] : '';
+            
+    //         if(!empty($title) && empty($status)){
+    //             $queryCondition = "and title like '%$title%'";
+    //         }
+    //         if(empty($title) && !empty($status)){
+    //             $queryCondition = "and opday <='$status'";
+    //         }
+    //         if(!empty($title) && !empty($status)) {
+    //             $queryCondition = "and title like '%$title%' and opday <='$status'";
+    //         }
+    //     }
+    //     $sql = "select no, title, DATE_FORMAT(inday, '%Y/%m/%d') AS insert_day, DATE_FORMAT(opday, '%Y/%m/%d') AS open_day, DATE_FORMAT(clday, '%Y/%m/%d') AS close_day
+    //     from topics where invalid = 0 " . $queryCondition . "
+    //     order by open_day desc";
+
+    //     $stmt = $conn->prepare($sql);
+
+    //     $result = execute($stmt, $conn);
+    //     if($result==TRUE)
+    //     {
+    //         $result = $stmt->get_result();
+    //         $resultSet = $result;
+    //     }
+
+    // }
+    // catch(ErrorException $ex)
+    // {
+    //     echo $ex->getMessage();
+    // }
+
+    ## Read value
+    $draw = isset($_POST['draw']) ? $_POST['draw'] : 1;
+    $row = isset($_POST['start']) ? $_POST['start'] : 0;
+    $rowperpage = isset($_POST['length']) ? $_POST['length'] : 10; // Rows display per page
+    // $columnIndex = $_POST['order'][0]['column']; // Column index
+    // $columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+    // $columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+    // $searchValue = $_POST['search']['value']; // Search value
+
+    ## Search 
+    $searchQuery = "";;
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $title = isset($_POST["title"]) ? $_POST["title"] : '';
+        $status = isset($_POST["status"]) ? $_POST["status"] : '';
+        
+        if(!empty($title) && empty($status)){
+            $searchQuery = "and title like '%$title%'";
+        }
+        if(empty($title) && !empty($status)){
+            $searchQuery = "and opday <= '$status'";
+        }
+        if(!empty($title) && !empty($status)) {
+            $searchQuery = "and title like '%$title%' and opday <= '$status'";
+        }
+        // echo $searchQuery;
+    }
+
+    ## Total number of records without filtering
+    $sel = mysqli_query($conn, "select count(*) as allcount from topics where invalid = 0");
+    $records = mysqli_fetch_assoc($sel);
+    $totalRecords = $records['allcount'];
+
+    ## Total number of records with filtering
+    $sel = mysqli_query($conn, "select count(*) as allcount from topics WHERE invalid = 0 " .$searchQuery);
+    $records = mysqli_fetch_assoc($sel);
+    $totalRecordwithFilter = $records['allcount'];
+
+    ## Fetch records
+    $empQuery = "select no, title, DATE_FORMAT(inday, '%Y/%m/%d') AS insert_day, DATE_FORMAT(opday, '%Y/%m/%d') AS open_day, DATE_FORMAT(clday, '%Y/%m/%d') AS close_day from topics WHERE invalid = 0 ".$searchQuery." order by open_day desc limit ".$row.",".$rowperpage;
+    
+    // echo $empQuery;
+    $empRecords = mysqli_query($conn, $empQuery);
+    $data = array();
+
+    while ($row = mysqli_fetch_assoc($empRecords)) {
+        $data[] = array(
+                "no" => $row['no'],
+                "insert_day" => $row['insert_day'],
+                "open_day" => $row['open_day'],
+                "close_day" => $row['close_day'],
+                "title" => $row['title']
+            );
+    }
+
+    ## Response
+    $response = array(
+        "draw" => intval($draw),
+        "iTotalRecords" => $totalRecords,
+        "iTotalDisplayRecords" => $totalRecordwithFilter,
+        "aaData" => $data
+    );
+
+    echo json_encode($response);
+    // die();
+?>
